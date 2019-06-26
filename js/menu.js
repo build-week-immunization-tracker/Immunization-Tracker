74233df94c77d70093bc8dea4ba55e3fd3743cfa
Robert Thompson <rthom2171@icloud.com>
2019-06-26 16:59:33 -0500

    Re-implmented nav-bar

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,22 @@
 
-const openButton = document.querySelector(".fa-vial");
+// const openButton = document.querySelector(".fa-vial");
 
-openButton.addEventListener("click", function(){
-    document.getElementById("navMenu").style.width = "250px";
-});
+// openButton.addEventListener("click", function(){
+//     document.getElementById("navMenu").style.width = "250px";
+// });
 
-const closeButton = document.querySelector(".closebtn");
+// const closeButton = document.querySelector(".closebtn");
 
-closeButton.addEventListener("click", function(){
-    document.getElementById("navMenu").style.width = "0px";
+// closeButton.addEventListener("click", function(){
+//     document.getElementById("navMenu").style.width = "0px";
+// });
+
+const menu = $(".mobile-links");
+
+$(".border").click(function(){
+  if(menu.is(":visible")){
+    menu.slideUp("slow");
+  } else {
+    menu.slideDown("slow");
+  }
 });
