4167acafb6ad69c5d848338ead143ea7e5fdad22
Robert Thompson <rthom2171@icloud.com>
2019-06-27 21:55:32 -0500

    Fixed styling on stories page

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,4 @@
 
-// const openButton = document.querySelector(".fa-vial");
-
-// openButton.addEventListener("click", function(){
-//     document.getElementById("navMenu").style.width = "250px";
-// });
-
-// const closeButton = document.querySelector(".closebtn");
-
-// closeButton.addEventListener("click", function(){
-//     document.getElementById("navMenu").style.width = "0px";
-// });
-
 const menu = $(".mobile-links");
 
 $(".border").click(function(){
