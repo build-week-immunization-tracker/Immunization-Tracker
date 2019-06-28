c20745d3d3d049cda957f4cc4103063974d85944
Robert Thompson <rthom2171@icloud.com>
2019-06-27 22:04:56 -0500

    Reformatted code

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,7 @@
-
 const menu = $(".mobile-links");
 
-$(".border").click(function(){
-  if(menu.is(":visible")){
+$(".border").click(function() {
+  if (menu.is(":visible")) {
     menu.slideUp("slow");
   } else {
     menu.slideDown("slow");
